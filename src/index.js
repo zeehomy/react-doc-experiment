6e4d6f15ec5c35aa5e19f5d10d875bf92c67586d
yuezihao <yuezihao@joinquant.com>
2020-07-16 11:21:26 +0800

    udpate experiment

--- src/index.js
+++ src/index.js
@@ -1,17 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+function formatName(user) {
+  return user.firstName + ' ' + user.lastName;
+}
+
+const user = {
+  firstName: 'Harper',
+  lastName: 'Perez',
+};
+
+// --------------------------
+
+// 在 JSX 中嵌入表达式；JSX 也是一个表达式
+// 在 JSX 中，大括号内可以放置任何有效的 JavaScript 表达式
+const element = (
+  <h1>Hello, {formatName(user)}!</h1>
+);
+const myElement = (
+  <div tabIndex="0"
+    className={user.lastName}
+  >sdfsfdsf {user.firstName}{element}</div>
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// ------------------------
+// 使用React.createElement()对JSX取值后得到 JavaScript 对象
+const ele = React.createElement(       // 代表JSX
+  'h1',
+  {
+    className: 'trade'
+  },
+  'xinjian'
+);
+
+ReactDOM.render(            // 第一个参数可以是JSX表达式，或组件
+  ele,
+  document.getElementById('root')
+);
