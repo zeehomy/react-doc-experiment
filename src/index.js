a88dc9420bae875bbf0f4935c3d99f175471aa32
yuezihao <yuezihao@troila.com>
2020-07-16 19:11:39 +0800

    update conditional rendering

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-function formatName(user) {
-  return user.firstName + ' ' + user.lastName;
-}
+// function formatName(user) {
+//   return user.firstName + ' ' + user.lastName;
+// }
 
-const user = {
-  firstName: 'Harper',
-  lastName: 'Perez',
-};
+// const user = {
+//   firstName: 'Harper',
+//   lastName: 'Perez',
+// };
 
 // --------------------------
 
@@ -147,37 +147,37 @@ const user = {
 // setInterval(tick, 1000);
 
 // 加入state使只写一次代码(有自己的状态)和生命周期钩子
-class Clock extends React.Component {
-  constructor(props) {
-    super(props);             // 将 props 传递到父类的构造函数中
-    this.state = {
-      date: new Date()
-    };
-  }
+// class Clock extends React.Component {
+//   constructor(props) {
+//     super(props);             // 将 props 传递到父类的构造函数中
+//     this.state = {
+//       date: new Date()
+//     };
+//   }
 
-  componentDidMount() {
-    this.timerID = setInterval(() => this.updateDate(), 1000);
-  }
+//   componentDidMount() {
+//     this.timerID = setInterval(() => this.updateDate(), 1000);
+//   }
 
-  componentWillUnmount() {
-    clearInterval(this.timerID);
-  }
+//   componentWillUnmount() {
+//     clearInterval(this.timerID);
+//   }
 
-  updateDate() {
-    this.setState({
-      date: new Date()
-    });
-  }
+//   updateDate() {
+//     this.setState({
+//       date: new Date()
+//     });
+//   }
 
-  render() {
-    return (
-      <div>
-        <h1>Hello, world!</h1>
-        <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
-      </div>
-    );
-  }
-}
+//   render() {
+//     return (
+//       <div>
+//         <h1>Hello, world!</h1>
+//         <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+//       </div>
+//     );
+//   }
+// }
 
 // ReactDOM.render(
 //   <Clock />,
@@ -186,17 +186,198 @@ class Clock extends React.Component {
 
 // -------------------------------------
 // 每个组件都是真正独立的
-function App() {
+// function App() {
+//   return (
+//     <div>
+//       <Clock />
+//       <Clock />
+//       <Clock />
+//     </div>
+//   );
+// }
+
+// ReactDOM.render(
+//   <App />,
+//   document.getElementById('root')
+// );
+
+// -----------------------------------
+// 事件 阻止默认
+// function ActionLink() {
+//   function handleClick(e) {
+//     e.preventDefault();
+//     console.log('The link was clicked.');
+//   }
+
+//   return (
+//     <a href="#" onClick={handleClick}>
+//       Click me
+//     </a>
+//   );
+// }
+
+// ReactDOM.render(
+//   <ActionLink />,
+//   document.getElementById('root')
+// );
+
+// ------------------------------------
+// JSX 的this
+// class Toggle extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {isToggleOn: true};
+
+//     // 为了在回调中使用 `this`
+//     this.handleClick = this.handleClick.bind(this);
+//   }
+
+//   handleClick() {
+//     this.setState(state => ({
+//       isToggleOn: !state.isToggleOn
+//     }));
+//   }
+
+//   render() {
+//     return (
+//       <button onClick={this.handleClick}>
+//         {this.state.isToggleOn ? 'ON' : 'OFF'}
+//       </button>
+//     );
+//   }
+// }
+
+// ReactDOM.render(
+//   <Toggle />,
+//   document.getElementById('root')
+// );
+
+// -----------------------------------
+// 条件渲染
+// function UserGreeting(props) {
+//   return <h1>Welcome back!</h1>;
+// }
+
+// function GuestGreeting(props) {
+//   return <h1>Please sign up.</h1>;
+// }
+
+// function Greeting(props) {
+//   const isLoggedIn = props.isLoggedIn;
+//   if (isLoggedIn) {                         // 用if
+//     return <UserGreeting />;
+//   }
+//   return <GuestGreeting />;
+// }
+
+// // ReactDOM.render(
+// //   // Try changing to isLoggedIn={true}:
+// //   <Greeting isLoggedIn={false} />,
+// //   document.getElementById('root')
+// // );
+
+// function LoginButton(props) {
+//   return (
+//     <button onClick={props.handleLoginClick}>
+//       Login
+//     </button>
+//   );
+// }
+
+// function LogoutButton(props) {
+//   return (
+//     <button onClick={props.handleLogoutClick}>
+//       Logout
+//     </button>
+//   );
+// }
+
+// class LoginControl extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.handleLoginClick = this.handleLoginClick.bind(this);
+//     this.handleLogoutClick = this.handleLogoutClick.bind(this);
+//     this.state = {isLoggedIn: false};
+//   }
+
+//   handleLoginClick() {
+//     this.setState({isLoggedIn: true});
+//   }
+
+//   handleLogoutClick() {
+//     this.setState({isLoggedIn: false});
+//   }
+
+//   render() {
+//     const isLoggedIn = this.state.isLoggedIn;
+//     // let button = isLoggedIn
+//     //   ? <LogoutButton handleLogoutClick={this.handleLogoutClick} />
+//     //   : <LoginButton handleLoginClick={this.handleLoginClick} />; 
+
+//     return (                                      // 用三目运算符
+//       <div>
+//         <Greeting isLoggedIn={isLoggedIn} />
+//         { isLoggedIn
+//           ? <LogoutButton handleLogoutClick={this.handleLogoutClick} />
+//           : <LoginButton handleLoginClick={this.handleLoginClick} />
+//         }
+//       </div>
+//     );
+//   }
+// }
+
+// ReactDOM.render(
+//   <LoginControl />,
+//   document.getElementById('root')
+// );
+
+// ---------------------------------------
+// 阻止渲染
+function WarningBanner(props) {
+  if (!props.warn) {
+    return null;                    // 让组件返回null
+  }
+
   return (
-    <div>
-      <Clock />
-      <Clock />
-      <Clock />
-    </div>
-  );
+    <div className="warning">Warning!</div>
+  )
+}
+
+class Page extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showWarning: true
+    };
+    this.handleToggleClick = this.handleToggleClick.bind(this);
+  }
+
+  handleToggleClick(event) {
+    console.log('target', event.target);
+    console.log('currentTarget', event.currentTarget);
+    this.setState(state => ({
+      showWarning: !state.showWarning
+    }));
+  }
+
+  // 在组件的 render 方法中返回 null 并不会影响组件的生命周期。
+  componentDidUpdate() {
+    console.log('componentDidUpdate');
+  }
+
+  render() {
+    return (
+      <div>
+        <WarningBanner warn={this.state.showWarning} />
+        <button onClick={this.handleToggleClick}>
+          {this.state.showWarning ? 'Hide' : 'Show'}
+        </button>
+      </div>
+    )
+  }
 }
 
 ReactDOM.render(
-  <App />,
+  <Page />,
   document.getElementById('root')
 );
