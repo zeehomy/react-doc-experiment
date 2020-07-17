b45dbcc1d2da7dae510d1e8299dce9bf399a9d62
yuezihao <yuezihao@troila.com>
2020-07-17 15:04:18 +0800

    update temperature

--- src/index.js
+++ src/index.js
@@ -1,6 +1,94 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+// 状态提升
+// 多个组件需要反映相同的变化数据时，建议将共享状态提升到最近的共同父组件中
+function BoilingVerdict(props) {
+  const celsius = props.celsius;
+  return (
+    <>
+      {
+        celsius >= 100
+          ? <p>The water would boil.</p>
+          : <p>The water would not boil.</p>
+      }
+    </>
+    // 或者直接返回
+    // celsius >= 100
+    //   ? <p>The water would boil.</p>
+    //   : <p>The water would not boil.</p>
+  );
+}
+
+const scaleNames = {
+  c: 'Celsius',
+  f: 'Fahrenheit'
+};
+
+class TemperatureInput extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+    this.state = {
+      temperature: ''
+    };
+  }
+
+  handleChange(e) {
+    this.setState({temperature: e.target.value});
+  }
+
+  render() {
+    const temperature = this.state.temperature;
+    const scale = this.props.scale;
+    return (
+      <fieldset>
+        <legend>Enter temperature in {scaleNames[scale]}:</legend>
+        <input value={temperature}
+          onChange={this.handleChange}
+        />
+      </fieldset>
+    );
+  }
+}
+
+// 温度转换函数
+function toCelsius(fahrenheit) {
+  return (fahrenheit - 32) * 5 / 9;
+}
+
+function toFahrenheit(celsius) {
+  return (celsius * 9 / 5) + 32;
+}
+
+function tryConvert(temperature, convert) {
+  const input = parseFloat(temperature);
+  if (Number.isNaN(input)) {
+    return '';
+  }
+  const output = convert(input);
+  const rounded = Math.round(output * 1000) / 1000;
+  return rounded.toString();
+}
+
+class Calculator extends React.Component {
+  
+  render() {
+    return (
+      <div>
+        <TemperatureInput scale="c" />
+        <TemperatureInput scale="f" />
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(
+  <Calculator />,
+  document.getElementById('root')
+);
+
+// ---------------------------------
 // 组合
 // function FancyBorder(props) {
 //   return (
